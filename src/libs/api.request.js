258053baaa4b6e909328b29515111f0a795c89ca
WYT <wangyt@winning.com.cn>
2019-08-02 14:05:19 +0800

    【重要的请求配置】修改开发环境和生产环境，路径

--- src/libs/api.request.js
+++ src/libs/api.request.js
@@ -1,6 +1,7 @@
 import HttpRequest from '@/libs/axios';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://rap2api.taobao.org/app/mock' : '';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://rap2api.taobao.org/app/mock' : 'http://rap2api.taobao.org/app/mock';
 const axios = new HttpRequest(baseUrl);
+// const baseUrl = 'http://rap2api.taobao.org/app/mock'
 
 export default axios;
